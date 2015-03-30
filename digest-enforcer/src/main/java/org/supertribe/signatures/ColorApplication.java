05fc18a603212a2bc3b768def145a176be2e4f7a
Jonathan Gallimore <jon@jrg.me.uk>
2015-03-30 15:21:49 +0100

    Adding digest enforcer sample

---
+++ digest-enforcer/src/main/java/org/supertribe/signatures/ColorApplication.java
@@ -0,0 +1,8 @@
+package org.supertribe.signatures;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ColorApplication extends Application {
+}
---
+++ digest-enforcer/src/main/java/org/supertribe/signatures/Colors.java
@@ -0,0 +1,29 @@
+/*
+ * Tomitribe Confidential
+ *
+ * Copyright(c) Tomitribe Corporation. 2014
+ *
+ * The source code for this program is not published or otherwise divested
+ * of its trade secrets, irrespective of what has been deposited with the
+ * U.S. Copyright Office.
+ */
+package org.supertribe.signatures;
+
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("colors")
+@Singleton
+@Lock(LockType.READ)
+public class Colors {
+
+    @POST
+    @Path("preferred")
+    public String preferredPost(final String c) {
+        return c;
+    }
+
+}
