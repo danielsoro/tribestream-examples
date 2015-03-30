1988b5fd05d0286f5b55cdad9be7ca547448c1b2
Jonathan Gallimore <jon@jrg.me.uk>
2015-03-30 11:40:51 +0100

    Docs for date-header-freshness

--- date-header-freshness/src/test/java/org/supertribe/signatures/ColorsTest.java
+++ date-header-freshness/src/test/java/org/supertribe/signatures/ColorsTest.java
@@ -36,8 +36,12 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Arquillian.class)
 public class ColorsTest {
 
-
-
+    /**
+     * Build the web archive to test. This adds in the KeystoreInitializer class from the test sources,
+     * which would otherwise be excluded.
+     * @return The archive to deploy for the test
+     * @throws Exception
+     */
     @Deployment(testable = false)
     public static WebArchive war() throws Exception {
         return new Mvn.Builder()
@@ -46,6 +50,10 @@ public class ColorsTest {
                 .addClass(KeystoreInitializer.class);
     }
 
+    /**
+     * Arquillian will boot an instance of Tribestream with a random port. The URL with the random port is injected
+     * into this field.
+     */
     @ArquillianResource
     private URL webapp;
 
@@ -55,6 +63,10 @@ public class ColorsTest {
     private Date oneHourAgo = new Date(System.currentTimeMillis() - TimeUnit.HOURS.toMillis(2));
     private String stringOneHourAgo = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US).format(oneHourAgo);
 
+    /**
+     * Tests accessing a signatures protected method with a GET request
+     * @throws Exception when test fails or an error occurs
+     */
     @Test
     public void success() throws Exception {
 
@@ -68,6 +80,10 @@ public class ColorsTest {
         assertEquals("orange", actual);
     }
 
+    /**
+     * Tests accessing a signatures protected method with a GET request with an invalid date
+     * @throws Exception when test fails or an error occurs
+     */
     @Test
     public void wrongDate() throws Exception {
 
@@ -81,6 +97,15 @@ public class ColorsTest {
         assertEquals(412, actual.getStatus());
     }
 
+    /**
+     * Create a digital signature using the HTTP method and request URI. This uses the shared secret constant
+     * from the KeystoreInitializer
+     *
+     * @param method HTTP method for the request (e.g. GET, POST, PUT etc)
+     * @param uri The URI of the request, e.g. "/colors/api/colors/preferred"
+     * @return The signature to set on the Authorization header.
+     * @throws Exception
+     */
     private Signature sign(final String method, final String uri, final String date) throws Exception {
         final Signature signature = new Signature(KeystoreInitializer.KEY_ALIAS, "hmac-sha256", null, "(request-target)", "date");
 
--- date-header-freshness/src/test/java/org/supertribe/signatures/KeystoreInitializer.java
+++ date-header-freshness/src/test/java/org/supertribe/signatures/KeystoreInitializer.java
@@ -21,6 +21,18 @@ public class KeystoreInitializer {
     public static final String ALGO = "HmacSHA256";
     private static File KS;
 
+    /**
+     * Initializes the keystore at ${TRIBESTREAM_HOME}/conf/test.jks, creating a keystore (with this is sensible ;-) as
+     * the keystore password), with a secret key of "this is supposed to be the shared secret between client and server.
+     * Not supposed to be in a constant." with an alias of "support".
+     *
+     * This is stored in the keystore with using the HmacSHA256 algorithm.
+     *
+     * This method is a @PostConstruct on a @Singleton @Startup EJB so the keystore is created as soon as the
+     * application starts up. This works great for a test, but is not the recommended approach in production.
+     *
+     * @throws Exception if an error occurs.
+     */
     @PostConstruct
     public void init() throws Exception {
         // init and generate a key
