50b6dd4f721251e9129a07eb659ff52826624adf
Jonathan Gallimore <jon@jrg.me.uk>
2015-03-31 11:11:09 +0100

    Copyright update

--- http-signatures-username-retriever/src/main/java/org/supertribe/signatures/SimpleUserNameRetriever.java
+++ http-signatures-username-retriever/src/main/java/org/supertribe/signatures/SimpleUserNameRetriever.java
@@ -1,7 +1,7 @@
 /*
  * Tomitribe Confidential
  *
- * Copyright(c) Tomitribe Corporation. 2015
+ * Copyright(c) Tomitribe Corporation. 2014
  *
  * The source code for this program is not published or otherwise divested
  * of its trade secrets, irrespective of what has been deposited with the
--- schedule-limit/src/main/java/org/supertribe/schedulelimit/WorkApplication.java
+++ schedule-limit/src/main/java/org/supertribe/schedulelimit/WorkApplication.java
@@ -1,3 +1,13 @@
+/*
+ * Tomitribe Confidential
+ *
+ * Copyright(c) Tomitribe Corporation. 2014
+ *
+ * The source code for this program is not published or otherwise divested
+ * of its trade secrets, irrespective of what has been deposited with the
+ * U.S. Copyright Office.
+ */
+
 package org.supertribe.schedulelimit;
 
 import javax.ws.rs.ApplicationPath;
